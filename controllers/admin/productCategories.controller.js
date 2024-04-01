2febccdd614bf5d22a2c430d210cf3136e2eb31e
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2024-04-01 12:15:44 +0700

    phân trang danh mục sp

--- controllers/admin/productCategories.controller.js
+++ controllers/admin/productCategories.controller.js
@@ -1,9 +1,15 @@
 const ProductCategory = require('../../models/product-category.model.js');
 
 const createTree = require('../../helpers/createTree.js');
+const searchHelper = require('../../helpers/search.js');
+const paginationHelper = require('../../helpers/pagination.js');
 
 // [GET] /admin/categories
 exports.index = async (req, res) => {
+
+  //! search
+  const search = searchHelper(req);
+  //! end search
 
   //! filter
   const filter = {};
@@ -15,14 +21,28 @@ exports.index = async (req, res) => {
   const find = {
     deleted: false,
     ...filter,
+    ...search
   };
 
+  //! pagination
+  const paginationObject = paginationHelper({
+    query: req.query,
+    limitItems: 5,
+    totalItems: await ProductCategory.countDocuments(find),
+  });
+  //! end pagination
+
   const categories = await ProductCategory
-    .find(find);
+    .find(find)
+    .sort({position: 1})
+    .skip(paginationObject.startItem)
+    .limit(paginationObject.limitItems);
 
   res.render('admin/pages/productCategories/index', {
     activeTab: 'categories',
     records: createTree(categories),
     filter,
+    searchKey: req.query.search,
+    paginationObject
   });
 };
